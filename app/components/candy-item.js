54723a0ef5c1eebd2b778a4bdde45ca3f22a2923
Shankar Sridhar <shsridhar@linkedin.com>
2018-08-08 09:07:56 -0700

    demo bugs when adding reference Types

--- app/components/candy-item.js
+++ app/components/candy-item.js
@@ -6,7 +6,7 @@ export default Component.extend({
 
   classNames: ['candy-item'],
 
-  isDiscountAllowed: computed.and('discount.percent', 'isOnSale', 'couponNotUsed'),
+  isDiscountAllowed: computed.and('discount.{couponNotUsed,percent}', 'isOnSale'),
 
   discountedPrice: computed('discount.percent', function() {
     const price = this.get('price');
@@ -14,21 +14,33 @@ export default Component.extend({
     return price * (1 - discountPercent);
   }),
 
-  couponNotUsed: true,
+  // DEMO: BAD - proto error
+  /* discount: {
+    couponNotUsed: true,
+    percent: 0.1
+  }, */
 
   init() {
     this._super(...arguments);
 
+    // DEMO: [GOOD] - proto error
     this.set('discount', {
+      couponNotUsed: true,
       percent: 0.1
     });
+    /* you may notice that this.discount = { } works fine here
+    this is ONLY possible in the init hook. Properties have their "finalized" values and become observable
+    after all of the init()s have run. 
+    Using this.discount = {} is a refactoring problem waiting to happen, because if you move that line to 
+    a didInsertElement or some other hook, you will now be circumventing the KVO api.
+    */
   },
 
   actions: {
     applyCoupon() {
       const discountedPrice = this.get('discountedPrice');
       this.set('isCouponApplied', true);
-      this.set('couponNotUsed', false);
+      this.set('discount.couponNotUsed', false);
       this.set('price', discountedPrice);
     }
   }
